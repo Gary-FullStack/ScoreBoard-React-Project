e681f6e7117e7d273bc27b3e42d9fd2a5d37c87b
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-02-02 12:35:34 -0500

    little more done today

--- app.js
+++ app.js
@@ -1,51 +1,86 @@
-const Header = () => {
+const players = [
+
+	{
+		name: "bob",
+		score: 50,
+		id: 1
+	},
+	{
+		name: "joe",
+		score: 85,
+		id: 2
+	},
+	{
+		name: "dude",
+		score: 95,
+		id: 3
+	},
+	{
+		name: "karen",
+		score: 80,
+		id: 4
+	}
+
+
+];
+
+
+
+const Header = (props) => {
 	return (
 		<header>
-			<h1>Scoreboard</h1>
-			<span className="stats">Players: 1</span>
+			<h1>{props.title}</h1>
+			<span className="stats">Players: {props.totalPlayers}</span>
 
 		</header>
 	);
 }
 
 
-const Player = () => {
+const Player = (props) => {
 	return (
 		<div className="player">
-			<span className="player-name">Bob</span>
-			<Counter />
+			<span className="player-name">{props.name}</span>
+			<Counter score={props.score} />
 		</div>
 	);
 }
 
-const Counter = () => {
+const Counter = (props) => {
 	return (
 
 		<div className="counter">
 			<button className="counter-action decrement"> - </button>
-			<span className="counter-score">1</span>
+			<span className="counter-score">{props.score}</span>
 			<button className="counter-action increment"> + </button>
 		</div>
 	);
 }
 
-const App = () => {
+const App = (props) => {
 	return (
-		<div className="apscoreboard">
+		<div className="scoreboard">
 
-			<Header />
+			<Header
+				title="React Project Scoreboard"
+				totalPlayers={props.initialPlayers.length} />
 
 			{/* Players list */}
-			<Player />
+			{props.initialPlayers.map(
+				player => <Player
+					name={player.name}
+					score={player.score}
+					key={player.id.toString()} />
+			)}
+
 		</div>
 
 	);
 }
 
 
-
 ReactDOM.render(
-	<App />,
+	<App initialPlayers={players} />,
 	document.getElementById('root')
 
 
